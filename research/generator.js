48f2b8c34a64210e53fd3b39c4f832375722129a
Raquel Van Hofwegen <raqvh@cs.washington.edu>
2016-01-30 10:50:11 -0800

    initial commit of js

---
+++ research/generator.js
@@ -0,0 +1,14 @@
+(function() {
+    "use strict";
+
+    window.onload = function () {
+    	// var generate = document.getElementById("generate");
+    	// var browse = document.getElementById("browse");
+        // var capBut = document.getElementById("cap-but");
+    };
+
+    function gettingStarted() {
+
+    }
+
+})();
