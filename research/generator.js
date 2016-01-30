41baeaa1b0bfe561d646134e215589be97697259
Raquel Van Hofwegen <raqvh@cs.washington.edu>
2016-01-30 11:54:50 -0800

    misspell in javascript

--- research/generator.js
+++ research/generator.js
@@ -3,17 +3,19 @@
 
     window.onload = function () {
         var generate = document.getElementById("gen");
-        gen.onclick = test;
+        generate.onclick = test;
+        console.log("hi");
     };
 
     function test() {
-    	var client = Algorithmia.client("sim1xkhR27Dzh5uFbgL17cFx0NC1");
-    	var input = 41;
-	    var client = Algorithmia.client("YOUR_API_KEY");
-	    client.algo("docs/JavaAddOne").pipe(input).then(function(output) {
-	    if(output.error) return console.error("error: " + output.error);
-	       console.log(output.result);
-	       document.getElementById("input_text").innerHTML = output.result;
+    	console.log("Hi");
+    	// var client = Algorithmia.client("sim1xkhR27Dzh5uFbgL17cFx0NC1");
+    	// var input = 41;
+	    // var client = Algorithmia.client("YOUR_API_KEY");
+	    // client.algo("docs/JavaAddOne").pipe(input).then(function(output) {
+	    // if(output.error) return console.error("error: " + output.error);
+	    //    console.log(output.result);
+	    //    document.getElementById("input_text").innerHTML = output.result;
     }
 
-})();
+}());
