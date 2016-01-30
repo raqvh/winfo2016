124aef696e47cb30ae38818d84090d222ed2c1f8
jack-arms <jack.armstrong121@gmail.com>
2016-01-30 14:38:14 -0800

    changed page

--- research/generator.js
+++ research/generator.js
@@ -14,48 +14,25 @@
 			.algo("algo://nlp/AutoTag/1.0.0")
 			.pipe(input)
 			.then(function(output) {
+				console.log(output.result);
 				getArticles(output.result);
 				document.getElementById("summary").innerHTML = output.result;
 			});
     }
 
-	function uploadText(event) {
+    function uploadText(event) {
+        console.log("uploading a file...");
         var reader = new FileReader();
         reader.onload = function(event) {
             var contents = event.target.result;
             document.getElementById("input_text").value = reader.result;
         };
+
         reader.onerror = function(event) {
             console.error("File could not be read! Code " + event.target.error.code);
         };
-        reader.readAsText(event.target.files[0]);
-	}
 
-	function addLinks(tags, list, index, count, seen) {
-		var input = {"search": tags[index]};
-		Algorithmia.client("sim1xkhR27Dzh5uFbgL17cFx0NC1")
-        .algo("algo://web/WikipediaParser/0.1.0")
-        .pipe(input)
-        .then(function(output) {
-        var j = 0;
-	        for(var i = 0; i < count; i++) {
-	        	if(seen.indexOf(output.result[j]) === -1 && output.result[j].indexOf("disambiguation") === -1) {
-		            var element = document.createElement("li");
-		            var wikiLink = "https://en.wikipedia.org/wiki/" + output.result[j].replace(" ", "_");
-		            var newLink = document.createElement("a");
-		            newLink.href = wikiLink;
-		            newLink.target = "_blank";
-		            element.appendChild(newLink);
-		            newLink.innerHTML = output.result[j];
-		            element.className = "list-group-item";
-		            list.appendChild(element);
-		            seen.push(output.result[j]);
-	        	} else {
-	        		i -= 1;
-	        	}
-	        	j++;
-	        }
-        });
+        reader.readAsText(event.target.files[0]);
 	}
 
     function getArticles(tags) {
@@ -64,24 +41,54 @@
     	var list = document.createElement("ul");
     	list.id = "linklist";
     	list.className = "list-group";
-    	var seen = [];
+    	for(var i = 0; i < 5; i++) {
+    		console.log(tags[i]);
 
-    	console.log("hi");
-    	addLinks(tags, list, 0, 2, seen);
-    	addLinks(tags, list, 1, 2, seen);
-    	addLinks(tags, list, 2, 1, seen);
-    	addLinks(tags, list, 3, 1, seen);
-    	console.log("end");
 
-		document.getElementById("col2").appendChild(list);
-    	
+    		var input = {"search": tags[i]};
+			Algorithmia.client("sim1xkhR27Dzh5uFbgL17cFx0NC1")
+           .algo("algo://web/WikipediaParser/0.1.0")
+           .pipe(input)
+           .then(function(output) {
+             console.log(output.result[0]);
+             
+             
+             
+           //  for(var i = 0; i < 5; i++) {
+             	//console.log("hi" + i);
+             	var element = document.createElement("li");
+             	var wikiLink = "https://en.wikipedia.org/wiki/" + output.result[0].replace(" ", "_");
+             	var newLink = document.createElement("a");
+             	newLink.href = wikiLink;
+             	newLink.target = "_blank";
+             	element.appendChild(newLink);
+             	newLink.innerHTML = output.result[0];
+             	element.className = "list-group-item";
+             	list.appendChild(element);
+             	console.log(output.result[0]);
+          //   }
+             
+           });
+    	}
+    	document.getElementById("col2").appendChild(list);
        }
-       
+
+      // var list = document.createElement("ul");
+      // list.className = "list-group";
+
+      // for(var i = 0; i < tags.length; i++) {
+      //   var element = document.createElement("li");
+      //   element.innerHTML = "#" + tags[i].replace(" ", "");
+      //   element.className = "list-group-item";
+      //   list.appendChild(element);
+      // }
+
+      // panel.appendChild(list);
       function clear() {
       	var col = document.getElementById("col2");
       	console.log(document.getElementById("linklist"));
       	var list = document.getElementById("linklist");
-      	if(list !== null) {
+      	if(list != null) {
       		col.removeChild(list);
       	}
       	document.getElementById("summary").innerHTML = "";
