37652ad61012d9c05b5eb37d0efbc1a8f64414f7
Raquel Van Hofwegen <raqvh@cs.washington.edu>
2016-01-30 13:09:41 -0800

    added links in the js

--- research/generator.js
+++ research/generator.js
@@ -13,6 +13,8 @@
 			.algo("algo://nlp/AutoTag/1.0.0")
 			.pipe(input)
 			.then(function(output) {
+				console.log(output.result);
+				getArticles(output.result);
 				document.getElementById("summary").innerHTML = output.result;
 			});
 		// client.algo("algo://demo/Hello/0.1.1")
@@ -21,5 +23,49 @@
   //       	document.getElementById("summary").innerHTML = output.result;
   //    	 });
     }
+
+    function getArticles(tags) {
+    	for(var i = 0; i < tags.length; i++) {
+    		console.log(tags[i]);
+    	}
+
+    	var input = {"search": "Great Filter"};
+		Algorithmia.client("sim1xkhR27Dzh5uFbgL17cFx0NC1")
+           .algo("algo://web/WikipediaParser/0.1.0")
+           .pipe(input)
+           .then(function(output) {
+             console.log(output.result[0]);
+             var links = document.getElementById("links");
+             var list = document.createElement("ul");
+             list.className = "list-group";
+             for(var i = 0; i < 5; i++) {
+             	//console.log("hi" + i);
+             	var element = document.createElement("li");
+             	var wikiLink = "https://en.wikipedia.org/wiki/" + output.result[i].replace(" ", "_");
+             	var newLink = document.createElement("a");
+             	newLink.href = wikiLink;
+             	newLink.target = "_blank";
+             	element.appendChild(newLink);
+             	newLink.innerHTML = output.result[i];
+             	element.className = "list-group-item";
+             	list.appendChild(element);
+             	console.log(output.result[i]);
+             }
+             document.getElementById("col2").appendChild(list);
+           });
+       }
+
+      // var list = document.createElement("ul");
+      // list.className = "list-group";
+
+      // for(var i = 0; i < tags.length; i++) {
+      //   var element = document.createElement("li");
+      //   element.innerHTML = "#" + tags[i].replace(" ", "");
+      //   element.className = "list-group-item";
+      //   list.appendChild(element);
+      // }
+
+      // panel.appendChild(list);
+  
 
 }());
